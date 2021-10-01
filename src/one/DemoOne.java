8d59a845ebeab350c2e6903ba8cd5459eef10e7a
Administrator <Administrator@Shams_Khalid.mshome.net>
2021-09-30 18:13:25 -0700

    new stuff

--- src/one/DemoOne.java
+++ src/one/DemoOne.java
@@ -6,6 +6,10 @@ public static void main (String []args) {
 		System.out.println("Demo");
 		
 		System.out.println("new added stuff");
+		System.out.println("new added stuff");
+
+		System.out.println("new added stuff");
+
 	}
 
 }
